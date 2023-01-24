dc56d06726ad3c4ebfe5fda32c789b4ac61101aa
CaptureB <airdavid62@gmail.com>
2023-01-24 07:34:50 -0700

    Worked on the server.js file, added localhost:PORT

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const htmlRoutes = require('./routes/htmlRoutes');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api', apiRoutes);
+app.use('/api', htmlRoutes
+)
+
 app.use(express.static('public'));
 
 
-
+app.listen(PORT, () => 
+    console.log(`App listening at https://localhost:${PORT} 🚀`)
+);
